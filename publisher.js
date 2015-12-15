a1f7d618494e6e1ccef300f6b6dc91af20d23a55
Ali Reza <alirezayahya@gmail.com>
2015-12-15 23:33:37 +0700

    failure attempt

--- publisher.js
+++ publisher.js
@@ -1,15 +1,25 @@
 var kue = require('kue'),
     queue = kue.createQueue()
 
+// Message payload
 var payload = {
     title: 'Welcome to JakartaJS'
   , to: 'hi@alileza.me'
   , template: 'welcome-email'
 }
 
-
 // Push jobs to queue named email
-var job = queue.create('email', payload).save( function(err){
-   if( !err )
-    console.log( job.id );
-})
+var job = queue.create('email', payload)
+              .priority('high')
+              .save( function(err){
+                   if( !err )
+                    console.log( job.id );
+              })
+
+// {
+//     low: 10
+//   , normal: 0
+//   , medium: -5
+//   , high: -10
+//   , critical: -15
+// };
