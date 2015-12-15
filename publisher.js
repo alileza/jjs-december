884ab5aa562cea153cdaa5c8f61a70dc5bd5b3f7
Ali Reza <alirezayahya@gmail.com>
2015-12-15 23:51:50 +0700

    add ttl

--- publisher.js
+++ publisher.js
@@ -9,23 +9,12 @@ var payload = {
 }
 
 // Push jobs to queue named email
-var job = queue.create('email', payload)
-              .priority('high')
-              .attempts(5)
-              .backoff( {delay: 60*1000, type:'fixed'} )
+var job = queue.create('email', payload)  
+              .ttl(15000)
               .save( function(err){
                    if( !err )
                     console.log( job.id );
               })
 
 
-// // Override delay value, fixed backoff
-// job.attempts(3).backoff( {delay: 60*1000, type:'fixed'} )
-//
-// // Enable exponential backoff using original delay (if set)
-// job.attempts(3).backoff( {type:'exponential'} )
-//
-// // Use a function to get a customized next attempt delay value
-// job.attempts(3).backoff( function( attempts, delay ){
-//  return my_customized_calculated_delay;
-// })
+// expiry value for the time their job can live in active state
