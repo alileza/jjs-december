7ef8c76abfa89af5021978746f6e46ed08203278
Ali Reza <alirezayahya@gmail.com>
2015-12-15 23:36:18 +0700

    set

--- publisher.js
+++ publisher.js
@@ -11,15 +11,21 @@ var payload = {
 // Push jobs to queue named email
 var job = queue.create('email', payload)
               .priority('high')
+              .attempts(5)
+              .backoff( {delay: 60*1000, type:'fixed'} )
               .save( function(err){
                    if( !err )
                     console.log( job.id );
               })
 
-// {
-//     low: 10
-//   , normal: 0
-//   , medium: -5
-//   , high: -10
-//   , critical: -15
-// };
+
+// // Override delay value, fixed backoff
+// job.attempts(3).backoff( {delay: 60*1000, type:'fixed'} )
+//
+// // Enable exponential backoff using original delay (if set)
+// job.attempts(3).backoff( {type:'exponential'} )
+//
+// // Use a function to get a customized next attempt delay value
+// job.attempts(3).backoff( function( attempts, delay ){
+//  return my_customized_calculated_delay;
+// })
